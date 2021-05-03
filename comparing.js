1e1e3a23e096b770bcc929cff99e56d6b46db843
hyunjinkr <a01020813765@gmail.com>
2021-05-03 20:34:09 +0900

    비교연산자.file

---
+++ comparing.js
@@ -0,0 +1,9 @@
+// console.log('사과'=='사과');
+// console.log(10 <30);
+// console.log(20 >=30);
+
+console.log(10 == '10');
+
+console.log(10 === '10');
+console.log(20 != '20');
+console.log(20 !== '20');
