f02de57bd3066c14bb4cbe7ded22b156e4877405
hyunjinkr <a01020813765@gmail.com>
2021-05-03 20:28:01 +0900

    상수사용.file

---
+++ const.js
@@ -0,0 +1,13 @@
+//상수 myname에 사자 대입
+// const myname = '사자';
+// //myname을 출력
+// console.log(myname);
+// const mystring = '사자';
+// const myfunction = () => console.log('함수확인');//이해가안됨
+
+// const myarray = ['펭귄','고래','참치'];
+// myarray[0] = '새우';
+// console.log(myarray);
+const myobject={id :20 , name :'펭귄'};
+myobject.name = '오리';
+console.log(myobject);
