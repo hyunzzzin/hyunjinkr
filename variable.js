9c0b0a4f5bde874f26a8f6b21910a62d63f88792
hyunjinkr <a01020813765@gmail.com>
2021-05-03 20:18:07 +0900

    변수선언.file

---
+++ variable.js
@@ -0,0 +1,28 @@
+// let mystring = '안현진'; // 문자열을 대입
+// let currentdate = new Date(); //현재 날짜를 대입
+// let myfunction = () => console.log('함수확인');//함수를 대입
+
+// console.log(mystring);
+// console.log(currentdate);
+// console.log();
+
+// let myname = '펭귄';
+// //myname = '고래';
+
+// console.log(myname);
+//숫자형 변수의 덧셈
+let number1 = 10;
+let number2 = 20;
+let sum = number1 + number2;
+console.log(sum);
+
+//문자열 변수의 결합
+let firstname = '안';
+let familyname = '현진';
+let fullname = firstname+familyname;
+console.log(fullname);
+
+//변수를 다른 변수에 대입
+let value1 = 100;
+let value2 = value1;
+console.log(value2);
