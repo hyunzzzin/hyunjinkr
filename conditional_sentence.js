7f61ea2dede001897685c84e9035f9012bec3d0b
hyunjinkr <a01020813765@gmail.com>
2021-05-04 18:44:04 +0900

    조건문if.file

---
+++ conditional_sentence.js
@@ -0,0 +1,23 @@
+//조건을 만족하는 데이터를 처리하고 싶을때
+/* syntax
+if (조건 1) {처리1} ->조건 1 true : 처리1 실행
+else if(조건2){처리2} -> 조건1 false, 조건2 true : 처리 2 실행 
+else{처리3} -> 조건1X, 조건2X : 처리 3 실행 */
+// const myprice = 4;
+
+// if (myprice >= 50){
+//     console.log('myprice는 50이상입니다.');
+// }else if (myprice >=10){
+//     console.log('myprice는 10이상입니다.');
+// }else {
+//     console.log('myprice는 10 미만입니다.');
+// }
+
+// const randomnum = math.random() * 10;
+// const randomnum = 5
+// if (randomnum >= 5){
+//     console.log('randomnum은 5이상');
+// } else{
+//     console.log('randomnum은 5미만');
+// }
+
