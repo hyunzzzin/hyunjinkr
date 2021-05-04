7d154b73e5a9290beb855d106332d76b541bba4f
hyunjinkr <a01020813765@gmail.com>
2021-05-04 17:47:32 +0900

    화살표함수.file

---
+++ arrowfunc.js
@@ -0,0 +1,25 @@
+//(파라미터) => {처리내용} : 함소를 정의
+//화살표함수를 사용하여 함수를 정의할 수 있다.
+//1.함수를 간략히 기술할 수 있다.
+// this를 묶는 것이 가능하다.
+
+// const calcSum = (a,b,c) => { // 원래같으면 function myfunction(a,b,c) {~~~} 이런걸 간략하게 줄임
+//     const result = a + b + c;
+//     return result;
+// }
+// console.log(calcSum(1, 1, 1));
+
+// const myfunction1 = (a) => {
+//     return a +2;
+// };
+
+// const myfunction2 = a =>{ // 파라미터 괄호 생략
+//     return a+2;
+// };
+
+// const myfunction3 = a => a+2; //화살표함수가 한줄이면 {}와 return을 생략가능
+
+// console.log(myfunction1(2));
+// console.log(myfunction2(2));
+// console.log(myfunction3(2)); // 출력문은 다 동일하게 나옴
+
