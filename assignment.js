5459148a72c91cee049bd8b8c09bfa4e4b127950
hyunjinkr <a01020813765@gmail.com>
2021-05-04 16:57:40 +0900

    복합대입연산자.file

---
+++ assignment.js
@@ -0,0 +1,14 @@
+// let a = 10;
+// let b = 20;
+// a += b; // a += b 는  a= a+b와 같이 사용된다
+// console.log(a); // a의 값 출력
+
+// let c = '자바';
+// let d = '스크립트';
+// c += d; // 문자열도 c = c+d , c는 자바스크립트가 합쳐져서 저장
+// console.log(c);
+
+// let e = 5;
+// let f = 2;
+// e *= f; // e= e*f
+// console.log(e);
