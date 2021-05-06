9e330c0cc936abcc60a3b662671b4d812629d75d
anhyunjin1 <a01020813765@gmail.com>
2021-05-07 00:06:14 +0900

    임의의수다루기.file

---
+++ random.js
@@ -0,0 +1,17 @@
+//임의의 수 다루기
+// 임의의 확률을 사용해 작업을 처리하고 싶을때
+//애니메이션에 임의의 값을 부여하고 싶을때
+
+//math.random() ->부동 소수점의 유사난수를 반환 (0이상1미만) -> 숫자
+
+// Math.random(); //0이상 1미만
+// Math.floor(math.random()*100);//0이상 100미만
+// 10 + math.floor(math.random()*10); //10이상 20미만
+
+//버튼을 누를때마다 색상을 임의로 바꾸는 코드를 통해 math.random()를 보자
+<!--botton-->
+<button class="button">색상 변경</button>
+<div class="rectangle"></div>
+.rectangle{
+    
+}
