ce06c7ef3a0cd0674de8d4b0867e29662c80f915
anhyunjin1 <a01020813765@gmail.com>
2021-05-07 00:05:49 +0900

    htmlbasic.file

---
+++ html.js
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="ko">
+    <head>
+        <meta charset="UTF-8">
+            <title></title>
+            <link rel="sytlesheet" href="style.css"/>
+            <script>
+                alert('안녕하세요');
+
+            </script>
+        </meta>
+    </head>
+    <body></body>
+</html>
