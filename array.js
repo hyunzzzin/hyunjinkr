0296dc5c75419e6b5f6f18e7e7ca928ae7a9f89f
anhyunjin1 <a01020813765@gmail.com>
2021-05-10 22:58:07 +0900

    배열정의.filr

---
+++ array.js
@@ -0,0 +1,18 @@
+// const array1 = []; //빈 배열
+// const array2 = [0,2,8];
+// console.log(array1,'\n',array2);
+
+// const array3 = ['곰','여우'];
+// const array4 = ['곰',1,false];
+// console.log(array4);
+
+
+// const array6 = [{ id:1 , name:'곰'} , { id:2 , name:'여우'}];
+// console.log(array6);
+
+const array = [1, 2, 3];
+console.log(array);
+
+const array8 = new Array(10);
+array8[0] = '곰';
+console.log(array8);
