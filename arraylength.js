8554128c54362b45be8e3be753b5a7f1dbb4787c
anhyunjin1 <a01020813765@gmail.com>
2021-05-10 23:02:57 +0900

    배열길이.file

---
+++ arraylength.js
@@ -0,0 +1,10 @@
+//배열 데이터 요소의 수를 확인하고 싶을때
+// 특정 json데이터 요소의 수를 확인하고 싶을때
+/* syntax
+배열.length -> 배열의 길이를 반환 */
+
+const array1 = ['곰','여우','사자'];
+console.log(array1.length);
+
+const array2 = [{id:1 , name:'appld'},{id:2 , name:'grape'}];
+console.log(array2[0][1]);
