cf1976d31e8bc625cd1dd70e1a071c60dbb6c728
hyunjinkr <a01020813765@gmail.com>
2021-05-13 18:18:15 +0900

    배열결합.file

---
+++ arrayjoin.js
@@ -0,0 +1,8 @@
+//배열 내 요소의 문자 데이터를 결합하고 싶을때
+//배열.join([결합문자열]) -> 배열 요소를 결합해 문자열 만들기
+
+const array1 = [2,4,10];
+console.log(array1.join()); //문자열
+
+const array2 = ['a','b','c'];
+console.log(array2.join('')); //abc
