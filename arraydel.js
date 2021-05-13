06cae84b9add78dad2efa348cda62e1cd5056324
hyunjinkr <a01020813765@gmail.com>
2021-05-13 17:27:38 +0900

    배열삭제.file

---
+++ arraydel.js
@@ -0,0 +1,18 @@
+//배열의 요소를 삭제하고싶을때
+/* 배열.shift() - >배열 첫 위치의 요소를 삭제
+배열.pop() - > 마지막 위치의 요소를 삭제 */
+
+// const array1 = [1,2,3];
+// const shiftedValue =  array1.shift();
+// console.log(shiftedValue);
+// console.log(array1);
+
+// const array2 = ['사과','귤','바나나'];
+// console.log(array2);
+// const popedValue = array2.pop();
+// console.log(popedValue);
+// console.log(array2);
+
+const array3 = [];
+const popedValue = array3.pop();
+console.log(array3);
