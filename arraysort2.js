6079ccccfff5e364cbddc2125b7cb74117806a32
anhyunjin1 <a01020813765@gmail.com>
2021-05-17 22:21:17 +0900

    객체포함_배열정렬.file

---
+++ arraysort2.js
@@ -0,0 +1,14 @@
+//객체를 포함하는 배열 정렬
+// 데이터
+// const userDataList =[
+//     {id:2, name: 'bare'},
+//     {id:10, name: 'fox'},
+//     {id:4, name: 'lion'},
+//     {id:29, name:'giraffe'},
+//     {id:101, name:'tiger'}
+// ];
+// console.log(userDataList[0]);
+
+const array1 = [5,1,10];
+array1.sort();
+console.log(array1);
