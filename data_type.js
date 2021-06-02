5dcf3b92e801a8e34d5323ab7e4751934cef8084
hyunjinkr <a01020813765@gmail.com>
2021-06-02 19:56:30 +0900

    데이터타입.file

---
+++ data_type.js
@@ -0,0 +1,15 @@
+//데이터 타입변환하기
+/* syntax
+boolean -> 진릿값 타입으로 변환 
+string -> 데이터를 문자열로 변환
+number -> 숫자타입으로 변환
+parseint -> 문자열을 숫자 타입으로 변환
+parseFloat -> 문자열을 숫자 타입으로 변환 */
+
+const result = 100 + Number('200');
+console.log(result);
+
+String(1);
+console.log(String(1));
+
+console.log(Number(true));
