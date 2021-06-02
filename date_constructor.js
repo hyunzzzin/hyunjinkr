e7fdc7dc04c052d47de7eb6d3d463012661ba576
hyunjinkr <a01020813765@gmail.com>
2021-06-02 21:03:32 +0900

    날짜_설정.file

---
+++ date_constructor.js
@@ -0,0 +1,18 @@
+//날짜 설정하기
+
+// const date1 = new Date('2021/06/02 20:01:10');
+// const date2 = new Date('mon june 02 2021 20:01:10');
+
+// console.log(date1);
+// console.log(date2);
+
+const date = new Date();
+
+date.setFullYear(2021);
+date.setMonth(0);
+date.setDate(1);
+date.setHours(0);
+date.setMinutes(0);
+date.setSeconds(0);
+
+console.log(date);
