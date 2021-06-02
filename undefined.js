43edff3558c95ebeb098ad4304f8980706ef42b9
hyunjinkr <a01020813765@gmail.com>
2021-06-02 20:01:04 +0900

    정의되지않는타입이해.file

---
+++ undefined.js
@@ -0,0 +1,13 @@
+//정의되지 않은 데이터 이해하기
+/* syntax
+Undefined -> 데이터가 정의되지않음 */
+
+let a;
+console.log(a);
+const object = {};
+console.log(object.b);
+
+function myFunction(c) {
+    console.log(`b의 값은 ${c}`);
+}
+myFunction();
